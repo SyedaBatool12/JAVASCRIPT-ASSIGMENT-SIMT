46d61a299cda8c39461a83081e7cc41557c3dc53
Syeda Batool Fatima <159137875+SyedaBatool12@users.noreply.github.com>
2024-06-05 18:47:31 +0500

    Add files via upload

---
+++ CHAPTER 3/app.js
@@ -0,0 +1,51 @@
+var age = 19;
+alert("I am " + age + " years old");
+
+
+var visitors = 0;
+alert("You have visited this site 14 times");
+
+
+var birthYear = 2005;
+document.write("My birth year is " + birthYear + "</br>");
+document.write("Data type of my declared variable is number");
+
+
+var store = "www.xyzClothing.com";
+var visitorName = prompt("Enter Your Name:")
+var productTitle = prompt("You have Ordered Product")
+var quantity = +prompt("Enter a Quantity of Product");
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on " + store);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
