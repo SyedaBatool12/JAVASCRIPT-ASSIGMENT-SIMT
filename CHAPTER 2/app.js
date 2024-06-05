61f928ec7a9360f361ec1caf3d3094e63c73bf56
Syeda Batool Fatima <159137875+SyedaBatool12@users.noreply.github.com>
2024-06-05 18:12:46 +0500

    Add files via upload

---
+++ CHAPTER 2/app.js
@@ -0,0 +1,24 @@
+var username;
+var myName = "SyedaBatoolFatima"; 
+var message;
+message = "Hello World";
+alert(message);
+var studentName = "SyedaBatoolFatima"; 
+var studentAge = "19 years old"; 
+var studentCourse = "Web development"; 
+ 
+alert("Student Name: " + studentName);
+alert("Student Age: " + studentAge);
+alert("Student Course: " + studentCourse);
+var testAlert = "This is a test alert";
+alert(testAlert);
+var email = "bsyeda707@g mail.com"; 
+alert("My email address is " + email);
+var book = "A smarter way to learn JavaScript";
+alert("I am learning from the book " + book);
+document.write("Yay! I can write HTML content through JavaScript");
+var displayString = "Hello, this is JavaScript";
+alert(displayString);
+document.write(displayString);
+
+
