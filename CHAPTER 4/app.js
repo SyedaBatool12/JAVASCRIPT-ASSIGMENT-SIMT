82f10ec5b65066b789473626811e1c969e33d4b2
Syeda Batool Fatima <159137875+SyedaBatool12@users.noreply.github.com>
2024-06-05 19:06:13 +0500

    Add files via upload

---
+++ CHAPTER 4/app.js
@@ -0,0 +1,15 @@
+
+var a,b,c
+
+var _batool;
+var _batool;
+var userName;
+var $ara;
+var num1;
+
+document.write("<h1>"+ "Rules for naming JS variables"  +"</h1>");
+document.write("<p>"+ "Variable names can only contain numbers, $ and _ . For example $my_1stVariable" +"</p>");
+document.write("<p>"+"Variables must begin with a letter,$ or _ . For example $name, _name or name"+"</p>");
+document.write("<p>"+"Variable names are case sensitive"+"</p>");
+document.write("<p>"+"Variable names should not be JS keywords"+"</p>");
+
